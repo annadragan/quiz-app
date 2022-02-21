391e4a6e176a5fb9ee018ce10f2075add972bf8f
Anna Dragan <anulkadragan@gmail.com>
2022-02-21 11:43:15 +0100

    Add counter

--- JS/counter.js
+++ JS/counter.js
@@ -20,28 +20,6 @@ function counter() {
     leftCharactersAnswer.innerText =
       createAnswer.maxLength - createAnswer.value.length;
   });
-  // const createQuestion = document.querySelector('[data-js="create-question"]');
-  // const leftCharactersQuestion = document.querySelector(
-  //   '[data-js="left-characters-question"]'
-  // );
-
-  // const createAnswer = document.querySelector('[data-js="create-answer"]');
-  // const leftCharactersAnswer = document.querySelector(
-  //   '[data-js="left-characters-answer"]'
-  // );
-
-  // createQuestion.addEventListener('input', () => {
-  //   const questionTextLength = createQuestion.value.length;
-  //   const maxQuestionTextLength = createQuestion.maxLength;
-  //   leftCharactersQuestion.innerText =
-  //     maxQuestionTextLength - questionTextLength;
-  // });
-
-  // createAnswer.addEventListener('input', () => {
-  //   const answerTextLength = createAnswer.value.length;
-  //   const maxLengthAnswer = createQuestion.maxLength;
-  //   leftCharactersAnswer.innerText = maxLengthAnswer - answerTextLength;
-  // });
 }
 
 export default counter;
